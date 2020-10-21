e067edb4a7db93d1ce748f38fd3732fcf906b032
John A. Leuenhagen <john@zlima12.com>
2020-10-21 16:58:09 -0400

    Add script in profile.d to check for dotfile updates.

---
+++ .local/etc/zsh/profile.d/95-check-updates.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+DOTFILES_DIR=~/.local/src/dotfiles
+
+cd "$DOTFILES_DIR" || exit 1;
+git status > /dev/null 2>&1 || exit 2;
+
+{
+	LOCAL="$(git rev-parse '@')" &&
+	REMOTE=$(git rev-parse '@{u}') &&
+	BASE=$(git merge-base "$LOCAL" "$REMOTE")
+} || exit 3;
+
+if [ "$LOCAL" = "$REMOTE" ]; then
+	: # Up to date.
+elif [ "$LOCAL" = "$BASE" ]; then
+	echo "You can update your dotfiles!";
+elif [ "$REMOTE" = "$BASE" ]; then
+	: # New unpushed commits.
+fi
