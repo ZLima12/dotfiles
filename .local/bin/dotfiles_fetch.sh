33d8b1c52392fdc4dd8e0e4a095cc2862a571a4a
John A. Leuenhagen <john@zlima12.com>
2020-10-21 16:20:45 -0400

    Add shell script, and systemd service/timer files to fetch dotfiles.

---
+++ .local/bin/dotfiles_fetch.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+DOTFILES_DIR=~/.local/src/dotfiles
+
+if SSH_AGENT_FILE="$(find ~/.local/share/keychain -name '*-sh' | head -1)"; then
+	. "$SSH_AGENT_FILE" ||
+		echo "Failed to source file: $SSH_AGENT_FILE";
+fi
+
+cd "$DOTFILES_DIR" || exit 1;
+git fetch --all;
